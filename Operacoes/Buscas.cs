8e07d4e6a8459b4238106a9680df884a17f43b65
Miguel St. F. Rodrigues <109636610+MiguelStFR@users.noreply.github.com>
2024-05-05 20:03:08 -0300

    1.1.2
    Correções

--- Operacoes/Buscas.cs
+++ Operacoes/Buscas.cs
@@ -47,7 +47,7 @@ namespace ProjetoGrafos.Operacoes
                         //visita aresta de árvore
                         if(vizinho.IndiceBusca == 0)
                         {
-                            Console.WriteLine("Visita aresta de pai {0} -> {1}", vertice, vizinho);
+                            Console.WriteLine("Visita aresta de pai {0} -> {1}", vertice.Tag, vizinho.Tag);
                             vizinho.PredecessorBusca = vertice;
                             vizinho.NivelBusca = vertice.NivelBusca ++;
                             tempo++;
@@ -57,15 +57,15 @@ namespace ProjetoGrafos.Operacoes
                         //visita aresta tio
                         else if(vizinho.NivelBusca == vertice.NivelBusca + 1)
                         {
-                            Console.WriteLine("Visita de aresta tio {0} -> {1}", vertice, vizinho);
+                            Console.WriteLine("Visita de aresta tio {0} -> {1}", vertice.Tag, vizinho.Tag);
                         }
                         else if(vizinho.NivelBusca == vertice.NivelBusca && vizinho.PredecessorBusca == vertice.PredecessorBusca && vizinho.IndiceBusca > vertice.IndiceBusca)
                         {
-                            Console.WriteLine("Visita de aresta irmão {0} -> {1}", vertice, vizinho);
+                            Console.WriteLine("Visita de aresta irmão {0} -> {1}", vertice.Tag, vizinho.Tag);
                         }
                         else if (vizinho.NivelBusca == vertice.NivelBusca && vizinho.PredecessorBusca != vertice.PredecessorBusca && vizinho.IndiceBusca > vertice.IndiceBusca)
                         {
-                            Console.WriteLine("Visita de aresta primo {0} -> {1}", vertice, vizinho);
+                            Console.WriteLine("Visita de aresta primo {0} -> {1}", vertice.Tag, vizinho.Tag);
                         }
                     }
                 }
@@ -76,13 +76,31 @@ namespace ProjetoGrafos.Operacoes
         {
             Console.WriteLine("INICIANDO BUSCA EM PROFUNDIDADE");
             tempo = 0;
+            Fila.Clear();
 
             for (int i = 0; i < verticeList.Count; i++)
             {
                 verticeList[i].TempoDescoberta = 0;
                 verticeList[i].TempoTermino = 0;
                 verticeList[i].PredecessorBusca = null;
+                Fila.Add(verticeList[i]);
             }
+
+            while (Fila.Find(v => v.TempoDescoberta == 0) != null)
+            {
+                Vertice vertice = Fila.Find(v => v.TempoDescoberta == 0);
+                if (vertice != null)
+                {
+                    int pos = Fila.IndexOf(vertice);
+                    BuscarEmProfundidade(pos);
+                }
+            }
+        }
+
+        public static void BuscarEmProfundidade(int pos)
+        {
+            tempo++;
+            
         }
     }
 }
